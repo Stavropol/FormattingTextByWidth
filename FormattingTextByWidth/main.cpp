67ebd22220047d1ba4dcbf04bf50d0f25cc2887e
Stavropol <107137189+Stavropol@users.noreply.github.com>
2023-06-09 21:18:54 +0300

    Переделана функция main для работы с функцией открытия файлов, а также подправлено название переменной входного файла.

--- FormattingTextByWidth/main.cpp
+++ FormattingTextByWidth/main.cpp
@@ -13,24 +13,22 @@ int main(int argc, char *argv[])
     QTextStream out(stdout);
     QString width = argv[1];
 
-    QString inputFilename = argv[2];
+    QString inputFileName = argv[2];
 
-    QFile inputFile(inputFilename);
-    if (!inputFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        qWarning() << "Не удалось открыть файл для чтения:" << inputFilename;
-        return -1;
+    QString inputText = readTxtFileToString(width.toInt(),inputFileName);
+    if (inputText.isEmpty()) {
+        return -1;  // Прерываем выполнение программы в случае ошибки
     }
-
-    QString inputText = QString::fromUtf8(inputFile.readAll());
-    inputFile.close();
 
     QString outputFilename = argv[3];
 
     QFile outputFile(outputFilename);
+
     if (!outputFile.open(QIODevice::WriteOnly | QIODevice::Text)) {
         qWarning() << "Не удалось открыть файл для записи:" << outputFilename;
         return -1;
     }
+
 
     QTextStream output(&outputFile);
     QString outputText = FormStringsFromWord(inputText, width.toInt());
