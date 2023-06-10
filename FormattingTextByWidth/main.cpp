bab1c74e9987ce5cf250d7b0bd191e43976baf91
Stavropol <107137189+Stavropol@users.noreply.github.com>
2023-06-10 03:21:11 +0300

    Добавлено описание программы для mainpage

--- FormattingTextByWidth/main.cpp
+++ FormattingTextByWidth/main.cpp
@@ -4,6 +4,32 @@
 #include <QDebug>
 #include "formattingtextbywidth.h"
 
+
+/*!
+*\file
+*\brief Файл содержит вызываемые функции программы, а также управляющую функцию.
+*\mainpage Документация для программы "FormattingTextByWidth"
+Программа должна формировать многострочный текст по ширине с расстановкой переносов и также минимизацией добавляемых пробелов.
+
+Для функционирования программы необходима операционная система Windows 8 или выше.
+Программа будет разработана на языке С++ с применением библиотек <string>, <QTextStream>, <QRegularExpression>, <QFile>, <QDebug>, <QCoreApplication>. Дополнительного программного обеспечения не требуется.
+Входные файлы должны быть подготовлены в редакторе Блокнот. Выходные файлы могут быть прочитаны с его помощью.
+
+Программа должна получать три параметра командной строки: ширину для форматирования текста, имя входного файла с исходным текстом и имя файла для записи текста.
+
+Пример команды запуска программы:
+*\code
+FormattingTextByWidth.exe 100 C:\input.txt  C:\output.txt
+*\endcode
+*\author Теликов Кирилл Евгеньевич
+*\date Июнь 2023 года
+*\version 1.0
+*/
+
+
+/**
+* @brief Обеспечивает запись в выходной файл, вывод в консоль ошибок, если они есть, вызов функций, решающих задачу.
+*/
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -16,7 +42,7 @@ int main(int argc, char *argv[])
     QString inputFileName = argv[2]; // Получение имени входного файла из аргумента командной строки.
 
     // Чтение содержимого входного файла
-    QString inputText = readTxtFileToString(width.toInt(),inputFileName);
+    QString inputText = ReadTxtFileToString(width.toInt(),inputFileName);
     if (inputText.isEmpty()) {
         return -1;  // Прерываем выполнение программы в случае ошибки
     }
